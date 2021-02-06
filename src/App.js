958a129675c8063e80607dfdef75bcbd8436e170
Ozod Khonberdiyev <khonberdiyev@gmail.com>
2021-02-06 23:37:26 +0100

    pushing first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react' 
+import {useEffect, useState} from 'react'
+import DisplayCoins from "./DisplayCoins"
 
 function App() {
+  const[coins, setCoins] = useState([])
+  const [search, setSearch] = useState("")
+
+let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
+
+useEffect(() => {
+   fetch(url)
+   .then(res => res.json())
+   .then(res => setCoins(res))
+},[])
+
+
+
+const handleChange = (e) =>{
+  setSearch(e.target.value)
+}
+
+const coinsFilter = coins.filter(coin =>{
+  return coin.name.toLowerCase().includes(search.toLowerCase())
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='search'>
+           <form>
+              <h1 className='header'>Crypto Market Info</h1>
+              <input type="text"  placeholder="Search here" onChange={handleChange}/>
+            </form>
+        
+         </div>
+       
+       <div className='main'>
+        {
+          coinsFilter.map(item => {
+           return (
+             <DisplayCoins data={item}  key={item.id}/>
+           )
+          })
+        }
+     
+
+       </div>
     </div>
   );
 }
---
+++ src/DisplayCoins.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function DisplayCoins(props) {
+   const {name, id, image, market_cap, price_change_percentage_24h, symbol, current_price} = props.data
+    
+return (
+
+    <div className='container'>
+        <div className='coinInfo'>
+
+            <div className='basic'>
+
+              <img src={image}/>
+
+              <h1>{name}</h1>
+
+              <p>{symbol.toUpperCase()}</p>
+              
+            </div>
+
+            <div className='details'>
+                <p className='price'>$ {current_price.toLocaleString()}</p>
+
+                {price_change_percentage_24h <0? (
+                    <p className='red'>{price_change_percentage_24h.toFixed(2)} %</p> )
+                     :  (<p className='green'>{price_change_percentage_24h.toFixed(2)} %</p>)
+                } 
+                <p>Mrt Cap: {market_cap.toLocaleString()}</p>
+            </div>
+              
+          </div>
+
+    </div>
+    
+    )
+}
+
+export default DisplayCoins
